8fe0d07b0171b639a33b500bd10df19c30fa8ff9
Cryplo <dylanliaa@gmail.com>
2024-04-13 00:05:23 -0400

    change random_walk function to end when steps are completed and not when origin is reached or steps are completed. Add commented out code for visualization

--- random_walk.py
+++ random_walk.py
@@ -1,13 +1,19 @@
-
 # %%
 import random
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+# declare some constant variables
+STEPS = 10000
+ITERATIONS = 10000
 
 # random walk function
 def random_walk(steps):
     x = 0  # x init to 0
     y = 0  # y init to 0
     
-    print(f"Initial position: ({x}, {y})")
+    #print(f"Initial position: ({x}, {y})")
     
     # iterate over "steps" amount of times
     for i in range(steps):
@@ -23,16 +29,37 @@ def random_walk(steps):
         elif direction == "down":
             y -= 1
         
-        print(f"Step {i + 1}: ({x}, {y})")
-        
-        # stop running if arrive back to start
-        if x == 0 and y == 0:
-            print(f"Arrived back to start: ({x}, {y})")
-            break
+        #print(f"Step {i + 1}: ({x}, {y})")
+    #print(f"Arrived at: ({x}, {y})")
 
-def main():
-    while input:
-        steps = int(input("Enter the number of steps for the random walk: "))
-        random_walk(steps)
+for i in range(ITERATIONS):
+    random_walk(STEPS)
+print("done")
+"""
+# test visualizing plots
 
-main()
+x_locations = range(0, STEPS)
+y_locations = range(0, STEPS)
+
+harvest = np.array([[0] * STEPS] * STEPS)
+
+fig, ax = plt.subplots()
+im = ax.imshow(harvest)
+
+# Show all ticks and label them with the respective list entries
+ax.set_xticks(np.arange(len(x_locations)), labels=x_locations)
+ax.set_yticks(np.arange(len(y_locations)), labels=y_locations)
+
+# Rotate the tick labels and set their alignment.
+plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+         rotation_mode="anchor")
+
+# Loop over data dimensions and create text annotations.
+for i in range(len(y_locations)):
+    for j in range(len(x_locations)):
+        text = ax.text(j, i, harvest[i, j],
+                       ha="center", va="center", color="w")
+
+ax.set_title("Harvest of local x_locations (in tons/year)")
+#fig.tight_layout()
+plt.show()"""
