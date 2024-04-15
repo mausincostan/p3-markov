496b68f29bd25ef837c3018740af699d9031c11c
Cryplo <dylanliaa@gmail.com>
2024-04-15 10:46:54 -0400

    Improve visualization by adding color bar, title, and put origin at bottom left of graph

--- random_walk.py
+++ random_walk.py
@@ -15,7 +15,7 @@ final_coordinates = []
 heatmap = [[0]*N for _ in range(N)]  # create a heatmap of n x n size
 
 # random walk function
-def random_walk(steps):
+def random_walk(steps: int) -> None:
     x = 0  # x init to 0
     y = 0  # y init to 0
     
@@ -46,45 +46,22 @@ def random_walk(steps):
     heatmap[y][x] += 1
 
 for i in range(ITERATIONS):
-    random_walk(STEPS)
+    random_walk(STEPS) if i % 2 == 0 else random_walk(STEPS + 1)
 
 x_locations = range(0, STEPS)
 y_locations = range(0, STEPS)
 
 heatmap_array = np.array(heatmap)
 
+
 fig, ax = plt.subplots()
-im = ax.imshow(heatmap_array, cmap = 'hot')
+im = ax.imshow(heatmap_array, cmap = 'hot') # cmap = 'autumn'
+ax.invert_yaxis()
+plt.xticks(range(0, N))
+plt.yticks(range(0, N))
+plt.colorbar(im)
+plt.title(f"Random Walk Simulation With {STEPS} Steps and {ITERATIONS} Iterations")
 plt.show()
 
 END_TIME = time.time()
 print(f"Time Elapsed: {END_TIME - START_TIME}s")
-
-"""
-# test visualizing plots
-
-x_locations = range(0, STEPS)
-y_locations = range(0, STEPS)
-
-harvest = np.array([[0] * STEPS] * STEPS)
-
-fig, ax = plt.subplots()
-im = ax.imshow(harvest)
-
-# Show all ticks and label them with the respective list entries
-ax.set_xticks(np.arange(len(x_locations)), labels=x_locations)
-ax.set_yticks(np.arange(len(y_locations)), labels=y_locations)
-
-# Rotate the tick labels and set their alignment.
-plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-         rotation_mode="anchor")
-
-# Loop over data dimensions and create text annotations.
-for i in range(len(y_locations)):
-    for j in range(len(x_locations)):
-        text = ax.text(j, i, harvest[i, j],
-                       ha="center", va="center", color="w")
-
-ax.set_title("Harvest of local x_locations (in tons/year)")
-#fig.tight_layout()
-plt.show()"""
