7c4b3e6901e982ad375b2f69970803c471b4b280
Cryplo <dylanliaa@gmail.com>
2024-04-14 23:16:43 -0400

    add visualization code, more logic for random walking, and log start and end time

--- random_walk.py
+++ random_walk.py
@@ -3,21 +3,34 @@ import random
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+import time
 
 # declare some constant variables
 STEPS = 10000
 ITERATIONS = 10000
+N = 10 # coordinates will range from 0 - 99 x 0 - 99
+START_TIME = time.time()
+
+final_coordinates = []
+heatmap = [[0]*N for _ in range(N)]  # create a heatmap of n x n size
 
 # random walk function
 def random_walk(steps):
     x = 0  # x init to 0
     y = 0  # y init to 0
     
-    #print(f"Initial position: ({x}, {y})")
-    
     # iterate over "steps" amount of times
     for i in range(steps):
-        direction = random.choice(["left", "right", "up", "down"])  # choose random direction
+        choices = []
+        if x > 0:
+            choices.append("left")
+        if x < N - 1:
+            choices.append("right")
+        if y > 0:
+            choices.append("down")
+        if y < N - 1:
+            choices.append("up")
+        direction = random.choice(choices)  # choose random direction
         # if x % n != x, reroll  
         # update coords
         if direction == "left":
@@ -29,12 +42,24 @@ def random_walk(steps):
         elif direction == "down":
             y -= 1
         
-        #print(f"Step {i + 1}: ({x}, {y})")
-    #print(f"Arrived at: ({x}, {y})")
+    final_coordinates.append([x, y])
+    heatmap[y][x] += 1
 
 for i in range(ITERATIONS):
     random_walk(STEPS)
-print("done")
+
+x_locations = range(0, STEPS)
+y_locations = range(0, STEPS)
+
+heatmap_array = np.array(heatmap)
+
+fig, ax = plt.subplots()
+im = ax.imshow(heatmap_array, cmap = 'hot')
+plt.show()
+
+END_TIME = time.time()
+print(f"Time Elapsed: {END_TIME - START_TIME}s")
+
 """
 # test visualizing plots
 
